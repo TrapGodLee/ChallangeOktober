2f5fd8cc45f1747efc34e0d22eab1bd3557abb1e
lesley <lreynolds@roc-dev.com>
2019-10-15 14:31:55 +0200

    challange

---
+++ src/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="monkey.css">
+    <title>Challenge</title>
+</head>
+<body>
+    <img class="monkey" src="image/81IBcKEw9XL._SL1500_.jpg" width="100" height="100" >
+    <img class="monkey" src="image/boots.jpg" width="100" height="100">
+</body>
+</html>
+
+<?php
